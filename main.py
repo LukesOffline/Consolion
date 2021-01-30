c0332be69635bb777d60b4268731d91f79cf437c
Luke <lukeharr2005@gmail.com>
2021-01-30 22:34:35 +0000

    Room generation v1

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 # This is the main game script.
 from Game.player import Player
-
-game_running = False
+from Game.roomManager import Room
 
 print("Welcome to Consolion, the open source console dungeon crawler written in Python")
+
 selected_name = input("What will your name be?")
 player = Player(selected_name)
-game_running = True
 
-if game_running:
-    print("Game running")
+running_game = True
+
+while running_game:
+    Room()
