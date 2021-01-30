b1d50d1eea1225ebe07e30fba05dbeb004948fc8
Luke <lukeharr2005@gmail.com>
2021-01-30 22:11:52 +0000

    Player class

---
+++ Game/player.py
@@ -0,0 +1,7 @@
+class Player:
+    health = 100
+    name = "Name here"
+
+    def __init__(self, input_name):
+        name = input_name
+        print(f"Your name is: {name}")
--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-# Lol
+# This is the main game script.
+from Game.player import Player
+
+game_running = False
+
+print("Welcome to Consolion, the open source console dungeon crawler written in Python")
+selected_name = input("What will your name be?")
+player = Player(selected_name)
+game_running = True
+
+if game_running:
+    print("Game running")
