e0c86023db3e725671cc8d4cbc296e2260ccbf6a
LukesOffline <lukeharr2005@gmail.com>
2021-03-04 10:33:45 +0000

    Update main.py

--- main.py
+++ main.py
@@ -5,9 +5,10 @@ from Game.roomManager import Room
 print("Welcome to Consolion, the open source console dungeon crawler written in Python")
 
 selected_name = input("What will your name be?")
-player = Player(selected_name)
+player = Player(selected_name) #For now this is cosmetic mostly, it's not really in use, could be in the future.
 
 running_game = True
 
 while running_game:
     Room()
+    
