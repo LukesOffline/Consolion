a2aed2e9c68c664b67ab0718e11f23f7c2782d13
LukesOffline <lukeharr2005@gmail.com>
2021-03-04 10:46:26 +0000

    Removed the room manager module

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 # This is the main game script.
+import sys
+import random
+
 from Game.player import Player
-from Game.roomManager import Room
+from Game.monster import Monster
 
 print("Welcome to Consolion, the open source console dungeon crawler written in Python")
 
@@ -10,5 +13,15 @@ player = Player(selected_name) #For now this is cosmetic mostly, it's not really
 running_game = True
 
 while running_game:
-    Room()
-    
+    print("You enter a room.")
+
+    you_find = random.randint(1, 2)
+    if you_find == 1:
+        rubies_found = random.randint(10, 50)
+        print(f"{player.name} has found {rubies_found} rubies!")
+    else:
+        Monster("Zombie")
+
+    cont_input = input("Type anything to continue, type end to exit.")
+    if cont_input == "end":
+        sys.exit()
